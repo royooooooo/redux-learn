97b17449638ea76655f281b53cdbc4ef65b8caee
Fu Yi <yifu@thoughtworks.com>
2019-06-20 14:57:45 +0800

    confirm whether given full state when call useState method

--- src/component/Father.tsx
+++ src/component/Father.tsx
@@ -1,17 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Children from "./Children";
 import Children1 from "./Children1";
 
 const Father = () => {
   let test = "child 2";
   const [count, setCount] = useState(0);
+  const [obj, setObj] = useState({ test1: "test", test2: "test2" });
+
+  useEffect(() => {
+    return () => {
+      console.log("effect");
+    };
+  });
 
   return (
     <div>
       <div>{count}</div>
-      <button onClick={() => {
-        setCount(count + 1)
-      }} >add 1</button>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        add 1
+      </button>
+      <div
+        onClick={() => {
+          setObj({ ...obj, test1: "test1" });
+        }}
+      >
+        {JSON.stringify(obj)}
+      </div>
     </div>
   );
 };
